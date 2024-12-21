f6236e0c136aa0d6abf59c6f21d82862e56d246d
baby-anjali <183719049+baby-anjali@users.noreply.github.com>
2024-12-21 21:43:21 +0530

    modified truthValuesOf function, tests passing

--- map.js
+++ map.js
@@ -36,7 +36,13 @@ const firstCharactersOf = function (strings) {
 
 // truth values of [0, 1, 2, 3] => [false, true, true, true]
 // Assume non-zero numbers are true, and zero is false
-const truthValuesOf = function (numbers) { };
+const truthValues = function (number) {
+  return number !== 0;
+};
+
+const truthValuesOf = function (numbers) {
+  return numbers.map(truthValues);
+};
 
 // // reverse strings of ["hello", "world"] => ["olleh", "dlrow"]
 // const reversedStringsOf = function (strings) { };
